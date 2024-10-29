68b145b0817d94ac139e29bf8fee3d0e37b055bb
NikitosekPro <148691930+NikitosekPro@users.noreply.github.com>
2024-10-29 18:46:12 +0100

    I made hero and yachts
    I hope it will work on github pages

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,36 @@
 $(function () {
-  $('').slick({
-    arrows: false,
-    dots: true,
-    fade: true,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    $('.yachts-list').slick({
+    arrows: true,
+    dots: false,
+    fade: false,
+    autoplay: false,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+            responsive: [
+        {
+            breakpoint: 2660,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 1280,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+
+  ]
   });
 });
+
+
