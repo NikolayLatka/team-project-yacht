7388beb1545e08c44693340ab3fda4714bd8ad8c
NikolayLatka <156698232+NikolayLatka@users.noreply.github.com>
2024-11-01 19:00:57 +0000

    Deploying to gh-pages from @ NikolayLatka/team-project-yacht@de3edc69e7db67b3c629b73d9533213af6af2a52 🚀

--- index.5b8ecb91.js
+++
@@ -1,2 +0,0 @@
-$((function(){$("").slick({arrows:!1,dots:!0,fade:!0,autoplay:!0,autoplaySpeed:3e3})}));
-//# sourceMappingURL=index.5b8ecb91.js.map
---
+++ index.af8b0f9f.js
@@ -0,0 +1,2 @@
+$((function(){$(".yachts-list").slick({arrows:!0,dots:!1,fade:!1,autoplay:!1,slidesToScroll:1,slidesToShow:1,responsive:[{breakpoint:2660,settings:{slidesToShow:3,slidesToScroll:1}},{breakpoint:1280,settings:{slidesToShow:2,slidesToScroll:1}},{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}}]})}));
+//# sourceMappingURL=index.af8b0f9f.js.map
