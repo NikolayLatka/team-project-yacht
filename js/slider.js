8378f292a59e2229d6c5cf3a86a254a2f908e2e4
KovalcnukArtem <art.ig.kov@gmail.com>
2024-11-06 20:27:24 +0200

    Add "customers" on tablet

--- js/slider.js
+++ js/slider.js
@@ -1,36 +1,68 @@
 $(function () {
-    $('.yachts-list').slick({
+  $('.yachts-list').slick({
     arrows: true,
     dots: false,
     fade: false,
     autoplay: false,
     slidesToScroll: 1,
     slidesToShow: 1,
-            responsive: [
-        {
-            breakpoint: 2660,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-            }
+    responsive: [
+      {
+        breakpoint: 2660,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
         },
-        {
-            breakpoint: 1280,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-            }
+      },
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
         },
-        {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        }
-
-  ]
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
 
+$(function () {
+  $('.rewiew-list').slick({
+    arrows: false,
+    dots: true,
+    fade: false,
+    autoplay: false,
+    slidesToScroll: 3,
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1279,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
 
+      {
+        breakpoint: 2660,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
