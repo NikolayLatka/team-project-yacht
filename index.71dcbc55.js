d289eedb07c7d23a0485b611bb6e7f3f24171cdc
NikitosekPro <148691930+NikitosekPro@users.noreply.github.com>
2024-11-06 18:30:59 +0000

    Deploying to gh-pages from @ NikolayLatka/team-project-yacht@1b44d33bbffa7900b8df8f87b568cbad7775c525 🚀

--- index.71dcbc55.js
+++
@@ -1,2 +0,0 @@
-$((function(){$(".yachts-list").slick({arrows:!0,dots:!1,fade:!1,autoplay:!1,slidesToScroll:1,slidesToShow:1,responsive:[{breakpoint:2660,settings:{slidesToShow:3,slidesToScroll:1}},{breakpoint:1280,settings:{slidesToShow:2,slidesToScroll:1}},{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}}]})}));
-//# sourceMappingURL=index.71dcbc55.js.map
